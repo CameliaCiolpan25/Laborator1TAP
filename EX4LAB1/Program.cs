ccee7296964b4f0737f43289307ee79ec7f4b507
User <User@DESKTOP-31H4JOE>
2022-02-24 18:56:58 +0200

    Add project files.

---
+++ EX4LAB1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String name = "Cami";
+Console.WriteLine("What's your name ?");
+Console.WriteLine("Cami");
+Console.WriteLine("Hello, Cami!Welcome to the .NET 6 world.");
---
+++ Laborator1TAP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Laborator1TAP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Cami";
+            Console.WriteLine("Hello Cami!Welcome to the .NET 5 world.");
+        }
+    }
+}
